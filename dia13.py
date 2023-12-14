f8b82d969a063bf6dd69e9583da24f829f70a2ef
Joao S O Bueno <gwidion@gmail.com>
2023-12-13 21:08:39 -0300

    Dia 13 completo

--- dia13.py
+++ dia13.py
@@ -18,6 +18,8 @@ class Indexable:
 
 
 class Map:
+    needed_difference = 0
+
     def __init__(self, data: str):
         self.data = data.split("\n")
         self.width = len(self.data[0])
@@ -26,6 +28,9 @@ class Map:
     def __getitem__(self, index):
         x, y = index
         return self.data[y][x]
+
+    def equals(self, first, second):
+        return (first == second), 0
 
     @Indexable
     def rows(self, index):
@@ -37,35 +42,54 @@ class Map:
 
     def check_reflection(self, index, sequence):
         had_run = False
+        smudges = 0
         for i, (j, forward_row) in zip(
             range(index, -1, -1),
             dropwhile(lambda pair: pair[0] <= index, enumerate(sequence))
         ):
             had_run = True
             backward_row = sequence[i]
-            if forward_row != backward_row:
+            if not (comparison:=self.equals(forward_row, backward_row))[0]:
                 return False
+            smudges += comparison[1]
+        if smudges != self.needed_difference:
+            return False
         return had_run
 
 
     def find_reflection(self):
         prev = ""
         for i, row in enumerate(self.rows):
-            if row == prev:
+            if self.equals(row, prev)[0]:
                 if self.check_reflection(i - 1, self.rows):
                     return i * 100
             prev = row
         prev = ""
         for i, col in enumerate(self.cols):
-            if col == prev:
+            if self.equals(col, prev)[0]:
                 if self.check_reflection(i - 1, self.cols):
                     return i
             prev = col
-        return RuntimeError("Map without a reflection line")
+        raise RuntimeError("Map without a reflection line")
 
     def  __repr__(self):
         return "\n".join(self.data)
 
 
+class MapPart2(Map):
+
+    needed_difference = 1
+
+    def equals(self, first, second):
+        if first == second:
+            return True, 0
+        differences = sum(c1 != c2 for c1, c2 in zip(first, second))
+        if differences <= self.needed_difference:
+            return True, differences
+        return False, 0
+
 def do_it_part1(data):
     return sum(Map(map).find_reflection() for map in data.split("\n\n"))
+
+def do_it_part2(data):
+    return sum(MapPart2(map).find_reflection() for map in data.split("\n\n"))
