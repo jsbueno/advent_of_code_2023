a4a3a224ff20dea27099e7a1680247ce7cef8158
João S. O. Bueno <gwidion@gmail.com>
2023-12-24 01:20:47 -0300

    Dia 19, part1: refactor - new 'Rule' class should work as base for part2

--- dia19.py
+++ dia19.py
@@ -1,4 +1,40 @@
 import re
+from operator import lt, gt
+
+RANGE_ALL = range(4000)
+
+class Rule:
+    def __init__(self, ruleset: dict, rule_line:str, rule_index=0):
+        self.ruleset = ruleset
+        self.line = rule_line
+        self.name, rules_str = rule_line.split("{")
+        self.index = rule_index
+        rule_str = rules_str.strip("}").split(",")[rule_index]
+        if ":" not in rule_str:
+            self.target = rule_str
+            self.type = "final"
+            self.register()
+            return
+        self.type = "cmp"
+        predicate, self.target = rule_str.split(":")
+        self.key = predicate[0]
+        self.op = gt if predicate[1]==">" else lt
+        self.threshold = int(predicate[2:])
+        self.register()
+        type(self)(ruleset, rule_line, rule_index + 1)
+
+    def register(self):
+        self.ruleset[self.name, self.index] = self
+
+    def __call__(self, part):
+        if self.type == "final":
+            return (self.target, 0)
+        if self.op(part[self.key], self.threshold):
+            return (self.target, 0)
+        return self.name, self.index + 1
+
+    def __repr__(self):
+        return f"Rule {self.index} of {self.line}"
 
 
 class XMASProcessor:
@@ -13,27 +49,15 @@ class XMASProcessor:
             for line in parts_str.split("\n")
         ]
 
-        self.rules = dict(self.rule_factory(line) for line in rules_str.split("\n"))
-
-    def rule_factory(self, rule_line):
-        name, rules_str = rule_line.split("{")
-        rules_str = rules_str.strip("}")
-        rule_chain = "lambda part: "
-        for rule_str in rules_str.split(","):
-            if ":" not in rule_str:
-                rule_chain += f' "{rule_str}"'
-                break
-            predicate, target = rule_str.split(":")
-
-            rule_chain += f' "{target}" if part["{predicate[0]}"] {predicate[1:]} else '
-        rule = eval(rule_chain)
-        return name, rule
+        self.rules = {}
+        for line in rules_str.split("\n"):
+            Rule(self.rules, line)
 
     def xunglepart(self, part):
-        target = "in"
-        while target not in "AR":
+        target = "in", 0
+        while target[0] not in "AR":
             target = self.rules[target](part)
-        return target
+        return target[0]
 
     def mungleallz(self):
         return [
@@ -41,11 +65,11 @@ class XMASProcessor:
                 if self.xunglepart(part) == "A"
         ]
 
-    def doit(self):
+    def doit_part1(self):
         return sum(sum(part.values()) for part in self.mungleallz())
 
-
+    doit = doit_part1
 
     def __repr__(self):
-        return f"{self.__class__.__name__} <{repr(self.rules)[:300]!s}...>"
+        return f"{self.__class__.__name__} <{len(self.rules)} rules>"
 
